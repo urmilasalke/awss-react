4535b02868d0094ac13496269179c345746c914b
urmilasalke <urmilasalke@gmail.com>
2021-12-06 16:24:44 +0530

    6th commit

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -6,13 +6,10 @@ import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Provider } from "react-redux";
 import { store } from "./components/store/AdmissionStore";
-import { BrowserRouter as Router } from "react-router-dom";
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router>
-        <App />
-      </Router>
+      <App />
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
