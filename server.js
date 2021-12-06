f4fe45a58be3ccb3d8bc9f9f9a8bf05886b0aef6
urmilasalke <urmilasalke@gmail.com>
2021-12-06 20:04:01 +0530

    server.js

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const path = require("path");
 const app = express();
 const cors = require("cors");
 dotenv.config();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "client", "build")));
+
 var corsOptions = {
   origin: "*",
 };
+
 app.use(cors(corsOptions));
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
@@ -31,13 +32,10 @@ mongoose
   .catch(() => {
     console.log("error");
   });
-app.use("/getdata", require("./routes/Admission"));
+
+app.use("/", require("./routes/Admission"));
 // app.use("/", require("./routes/Contactus"));
-app.get("/*", (req, res) => {
-  res.send("404 page not found");
-});
+
 app.listen(process.env.PORT, () => {
   console.log("Server is running");
 });
-
-module.exports = app;
