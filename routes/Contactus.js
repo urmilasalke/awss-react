8c75c82e1d559f17bcc1d03ef0ea1947c2825e6a
urmilasalke <urmilasalke@gmail.com>
2021-12-10 09:56:06 +0530

    swagger post method for contact us

--- routes/Contactus.js
+++ routes/Contactus.js
@@ -1,5 +1,88 @@
 const router = require("express").Router();
 const contactUs = require("../model/Contactus");
+
+/**
+ * @swagger
+ * definitions:
+ *  Contactus:
+ *   type: object
+ *   properties:
+ *    firstName:
+ *     type: string
+ *     description: first name of the user
+ *     example: 'urmila'
+ *    lastName:
+ *     type: string
+ *     description: last name of the user
+ *     example: 'salke'
+ *    email:
+ *     type: string
+ *     description: email of the user
+ *     example: 'urmilasalke@gmail.com'
+ *    message:
+ *     type: string
+ *     description: Message
+ *     example: 'Want to take admission'
+ */
+
+/**
+ * @swagger
+ * /contactus/:
+ *  post:
+ *   summary: create contact us
+ *   description: contact us
+ *   parameters:
+ *    - in: body
+ *      name: body
+ *      required: true
+ *      description: body of the team
+ *      schema:
+ *       $ref: '#/definitions/Contactus'
+ *   requestBody:
+ *    content:
+ *     application/json:
+ *      schema:
+ *       $ref: '#/definitions/Contactus'
+ *   responses:
+ *    200:
+ *     description: data saved succesfully
+ *    500:
+ *     description: failure while saving data
+ */
+
+// /**
+//  * @swagger
+//  * /contactus/:
+//  *   post:
+//  *     summary: Create a JSONPlaceholder user.
+//  *     requestBody:
+//  *       required: true
+//  *       content:
+//  *         application/json:
+//  *           schema:
+//  *             type: object
+//  *             properties:
+//  *               firstName:
+//  *                 type: string
+//  *                 description: The user's name.
+//  *                 example: 'Leanne Graham'
+//  *               lastName:
+//  *                 type: string
+//  *                 description: The user's name.
+//  *                 example: 'Leanne Graham'
+//  *               email:
+//  *                 type: string
+//  *                 description: The user's name.
+//  *                 example: 'Leanne Graham'
+//  *               message:
+//  *                 type: string
+//  *                 description: The user's name.
+//  *                 example: 'Leanne Graham'
+//  *
+//  *     responses:
+//  *       200:
+//  *         description: success
+//  */
 router.post("/", (req, res) => {
   console.log("inside admission");
   newContactus = new contactUs({
